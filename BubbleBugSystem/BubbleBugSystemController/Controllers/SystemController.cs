7d668cfa90145d7e1c8a55da039ea5407bb38f03
Александр Кравченков <LastSprint@yandex.ru>
2018-01-08 17:13:59 +0300

    Пересоздал проекты под REST Api

---
+++ BubbleBugSystem/BubbleBugSystemController/Controllers/SystemController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BubbleBugSystemController.Controllers {
+    [Route("api/[controller]")]
+    public class SystemController : Controller {
+
+        /// <summary>
+        /// Запускает генерацию мокового сервера.
+        /// </summary>
+        /// <param name="pathToSpecification">Путь к файлу спецификации</param>
+        /// <returns></returns>
+        [HttpPost]
+        public object StartGenerator(string pathToSpecification) {
+
+            if (!System.IO.File.Exists(pathToSpecification)) {
+                return new BadRequestResult();
+            }
+
+            return new CreatedResult(this.HttpContext.Connection.LocalIpAddress.ToString(), "Server was created");
+        }
+    }
+}
--- BubbleBugSystem/BubbleBugSystemController/Program.cs
+++ BubbleBugSystem/BubbleBugSystemController/Program.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
-namespace BubbleBugServerContainerController
+namespace BubbleBugSystemController
 {
     public class Program
     {
